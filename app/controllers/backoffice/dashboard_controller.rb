883d6dea267c55eddc29db24688416596cf65147
Sérgio Alonso Jr <SergioAwoke@gmail.com>
2021-10-22 20:58:19 -0300

    Revert "Feature/qtdd de horario reservado"

--- app/controllers/backoffice/dashboard_controller.rb
+++ app/controllers/backoffice/dashboard_controller.rb
@@ -2,10 +2,5 @@ class Backoffice::DashboardController < BackofficeController
 	helper 'backoffice/dashboard'
 
 	def index
-		@todays_appointments = Appointment.todays_appointments
-		@users = User.all
-		@rooms = Room.all
-		@appointments = Appointment.all
-		@appointments_aprove = Appointment.where(status: 2)
 	end
 end
