ea00f549f9b392cac55614aa69b0033fa221fef1
Smylle3 <joao21731@gmail.com>
2021-11-04 23:16:19 -0300

    Documentação qtdd de horarios reservados

--- app/controllers/backoffice/dashboard_controller.rb
+++ app/controllers/backoffice/dashboard_controller.rb
@@ -1,9 +1,8 @@
 class Backoffice::DashboardController < BackofficeController
 	helper 'backoffice/dashboard'
-	#before_action :signed_in?
-	#before_action :is_admin?, only: [:new, :create, :destroy]
-
 	def index
+		##
+		# Variável @accepted_appointments que recebe a quantidade de horarios reservados confirmadas pelo usuário administrador
 		@todays_appointments = Appointment.todays_appointments
 		@users = User.where(is_admin: false)
 		@rooms = Room.all
